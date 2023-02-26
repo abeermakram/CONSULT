22055c56bffc659852a51aa1e89be4f1b0a148c1
abeermakram <125794844+abeermakram@users.noreply.github.com>
2023-02-26 10:52:40 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// console.log(1000);
+// console.log("abood");
+// console.log(["abood",19,"teser"]);
+// console.log({name:"abood" , age:22});
+// console.log(typeof(10003488));
+// let x = 40;
+// let y=60;
+// let z = x+y;
+// console.log(z);
+
+// console.log(" Hello abood  \" from js \" ");
+
+// let farestname ="abood";
+// let lastname ="teser";
+// let space ="  ";
+// let follname = farestname +space+ lastname;
+// console.log(follname);
+
+// // let card = `<div>
+// // <h1>Hello</h1>
+// // </div>`
+
+// // document.write(card);
+
+
+
+// let titalCard= "javascript";
+// let descriptionCard= "i love javascript";
+// let dateCard= "24/1/20223";
+// let card =`<div>
+//   <h3>${titalCard}</h3>
+//   <p> ${descriptionCard} </p>
+//   <span>${dateCard}</span>
+// </div>`;
+
+// document.write( tital1 ,tital2 ,tital3 ,tital4 );
+// for(let i =0; i < 4 ; i++ ){
+//     document.write(card);
+// };
+
+
+// let number1=30;
+// let number2=10;
+
+// console.log(number1+number2);
+// console.log(number1-number2);
+// console.log(number1/number2);
+// console.log(number1*number2);
+// console.log(number1 % number2);
+
+// let x=3.5;
+// let y=5.4;
+
+// console.log(mathe.round(x));
+// console.log(mathe.round(y));
+
+
+// let x="abood";
+// console.log(x.charAt(1).trim().toLocaleUpperCase());
+
+let x="abood";
+console.log("a",0);
